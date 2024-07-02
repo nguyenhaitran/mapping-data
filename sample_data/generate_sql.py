f8e554fed13f846fbcc7389e481a3ed1018dbc9c
henrytran <126839412+nguyenhaitran@users.noreply.github.com>
2024-07-02 15:23:03 +0800

    insert values from csv file into sql file

--- sample_data/generate_sql.py
+++ sample_data/generate_sql.py
@@ -1,9 +1,12 @@
 import pandas as pd
 
 df = pd.read_csv("wine-ratings-small.csv", index_col=0)
+# Below is the sql template of inserting data into database
+# The %s is the empty values which will be replace with real values from csv file
 sql_tmpl = """INSERT INTO ratings(name, rating, region) VALUES("%s", "%s", "%s");\n"""
+#create a sql file and the mode is append
 sql_file = open("populate.sql", "a")
 
-for _, row in df.iterrows():
+for _, row in df.iterrows(): #loop through index and values
     sql_file.write(sql_tmpl % (row['name'], row['rating'], row['region']))
     #print(row)
